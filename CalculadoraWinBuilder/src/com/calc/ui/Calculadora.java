dca33b7f9cf8ca65bf44392f00460eea7d69552a
alex <alejandrocordobarodriguez@gmail.com>
2025-10-27 22:31:20 +0100

    Merge branch 'main' of https://github.com/Alrol-5689/dam-2-desarrollo-de-interfaces.git into main

--- CalculadoraWinBuilder/src/com/calc/ui/Calculadora.java
+++ CalculadoraWinBuilder/src/com/calc/ui/Calculadora.java
@@ -67,7 +67,7 @@ public class Calculadora extends JFrame {
 	public Calculadora() {
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 445, 507);
+		setBounds(100, 100, 290, 444);
 		
 		// contentPane
 		
