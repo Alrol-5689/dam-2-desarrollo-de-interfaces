11a4a2927e458ae14d6d4007adfcd9c096407fb2
alex <alejandrocordobarodriguez@gmail.com>
2025-10-27 18:25:43 +0100

    Finalizado

--- CalculadoraWinBuilder/src/com/calc/model/Operation.java
+++ CalculadoraWinBuilder/src/com/calc/model/Operation.java
@@ -2,26 +2,32 @@ package com.calc.model;
 
 public class Operation {
 	
-	public double multiplication(double num1, double num2) {
-		return num1 * num2;
-	}
 	
-	public double division(double num1, double num2) {
-		double result = 0;
-		try {
-			result = num1 / num2;			
-		}catch(Exception e) {
-			e.getMessage();
-		}
-		return result;
-	}
+//===>>	AL FINAL MANEJO LAS OPERACIONES TAMBIÉN DESDE LA UI 
 	
-	public double sum(double num1, double num2) {
-		return num1 + num2;
-	}
 	
-	public double subtraction(double num1, double num2) {
-		return num1 - num2;
-	}
+	
+	
+//	public double multiplication(double num1, double num2) {
+//		return num1 * num2;
+//	}
+//	
+//	public double division(double num1, double num2) {
+//		double result = 0;
+//		try {
+//			result = num1 / num2;			
+//		}catch(Exception e) {
+//			e.getMessage();
+//		}
+//		return result;
+//	}
+//	
+//	public double sum(double num1, double num2) {
+//		return num1 + num2;
+//	}
+//	
+//	public double subtraction(double num1, double num2) {
+//		return num1 - num2;
+//	}
 
 }
