90f3bb4994b81270c761b8022e2c33e8d771ba00
alex <alejandrocordobarodriguez@gmail.com>
2025-10-31 00:54:45 +0100

    pongo la versión dev en el JpaUtil

--- Ev_Tri1_AlejandroCordoba/src/main/java/com/primertrimestre/persistence/util/JpaUtil.java
+++ Ev_Tri1_AlejandroCordoba/src/main/java/com/primertrimestre/persistence/util/JpaUtil.java
@@ -18,7 +18,7 @@ public class JpaUtil {
             synchronized (JpaUtil.class) {
                 result = emf;
                 if (result == null) {
-                    result = Persistence.createEntityManagerFactory("EvPrimerTrimestrePU");
+                    result = Persistence.createEntityManagerFactory("EvPrimerTrimestrePU-dev");
                     emf = result;
                 }
             }
