91195dc3c9d96afa428da8b3a881032a51541dd8
alex <alejandrocordobarodriguez@gmail.com>
2025-10-31 00:55:57 +0100

    Creo Module.java y sus relaciones con Student.java para la persistencia

--- Ev_Tri1_AlejandroCordoba/src/main/java/com/primertrimestre/model/Module.java
+++ Ev_Tri1_AlejandroCordoba/src/main/java/com/primertrimestre/model/Module.java
@@ -1,5 +1,51 @@
 package com.primertrimestre.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity 
+@Table(name = "modules")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Module {
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private int creditsECTS; 
+	
+	@Column(nullable = false)
+	private String name;
+	
+	//@Setter(AccessLevel.NONE) No hace falta porque al escribir el setter lombok lo ignora
+	@Column(nullable = false)
+	private String shortName;
+	
+	@ManyToMany(mappedBy = "modules", fetch = FetchType.LAZY)
+	private Set<Student> students = new HashSet<>();
+	
+	public void setShortName(String shortName) {this.shortName = shortName.toUpperCase();} // Lombok respeta este setter y no lo sobreescribe
+	
+	
+	
 
 }
--- Ev_Tri1_AlejandroCordoba/src/main/java/com/primertrimestre/model/Student.java
+++ Ev_Tri1_AlejandroCordoba/src/main/java/com/primertrimestre/model/Student.java
@@ -1,11 +1,20 @@
 package com.primertrimestre.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,14 +29,26 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Student {
 	
-	@Id
+	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name = "username", nullable = false)
+	@Column(nullable = false)
 	private String username;
 	
-	
+	@Column(nullable = false)
 	private String password;
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(
+	  name = "student_modules",
+	  joinColumns = @JoinColumn(
+			  name = "student_id",
+			  foreignKey = @ForeignKey(name = "fk_student_modules_students")),
+	  inverseJoinColumns = @JoinColumn(
+			  name = "module_id",
+			  foreignKey = @ForeignKey(name = "fk_student_modules_modules")),
+	  uniqueConstraints = @UniqueConstraint(columnNames = {"student_id","module_id"}))
+	private Set<Module> modules = new HashSet<>();
 
 }
